3d97e758728bfd7052ee72df7ab3f182e3cc322f
Luke Walsh <lhwalsh@cpp.edu>
2018-02-08 14:30:44 -0800

    first commit

---
+++ server.java
@@ -0,0 +1,48 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OuptutStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class driver {
+
+    public final static int SOCKET_PORT = 13267;
+    public final static String FILE_TO_SEND = "test.txt";
+
+    public static void main(String args[]) {
+	FileInputStream fis = null;
+	BufferedInputStream bis = null;
+	OutputStream os = null;
+	ServerSocket servsock = null;
+	Socket sock = null;
+	try {
+	    servsock = new ServerSocket(SOCKET_PORT);
+	    while (true) {
+		System.out.println("Waiting...");
+	        try {
+		    sock = servsock.accept();
+		    System.out.println("Accepted connection : " + sock);
+		    File myFile = new File(FILE_TO_SEND);
+		    byte[] mybytearray = new byte[(int)myFile.length()];
+		    fis = new FileInputStream(myFile);
+		    bis.read(mybytearray, 0, mybytearray.length);
+		    os = sock.getOutputStream();
+		    System.out.println("Sending " + FILE_TO_SEND + "(" + mybitearray.length + " bytes)");
+		    os.write(mybytearray, 0, mybytearray.length);
+		    os.flush();
+		    System.out.println("Done.");
+		}
+		finally {
+		    if (bis != null) bis.close();
+		    if (os != null) os.close();
+		    if (sock != null) sock.close();
+		}
+	    }
+	}
+	finally {
+	    if (servsock != null) servsock.close();
+	}
+    }
+}
