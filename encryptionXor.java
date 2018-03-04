ff307d22a1cf5139c94533da0ff2f775f71e9adb
Khoa Vo <khoavo@cpp.edu>
2018-03-03 22:45:11 -0800

    EncryptionXor to bytes instead of String
    Changed Encryption to handle bytes instead of Strings.

--- encryptionXor.java
+++ encryptionXor.java
@@ -1,11 +1,12 @@
 public class encryptionXor { 
-    private static int[] encrypt(String str, String key) {
-        int[] output = new int[str.length()];
-        for(int i = 0; i < str.length(); i++) {
-            int o = (Integer.valueOf(str.charAt(i)) ^ Integer.valueOf(key.charAt(i % (key.length() - 1)))) + '0';
-            output[i] = o;
-        }
-        return output;        
+    private static byte[] encrypt(byte[] buffer, String key) {
+   	 byte[] keyBytes = key.getBytes();
+       byte[] output = new byte[buffer.length];
+       for(int i = 0; i < buffer.length; i++) {
+           byte o = (byte) (buffer[i] ^ keyBytes[i % keyBytes.length]);
+           output[i] = o;
+       }
+       return output;        
     }
 
     private static String decrypt(int[] input, String key) {
@@ -16,15 +17,31 @@ public class encryptionXor {
         return output;
     }
     
+    public static byte[] hexStringToByteArray(String s) {
+       int len = s.length();
+       byte[] data = new byte[len / 2];
+       for (int i = 0; i < len; i += 2) {
+           data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                + Character.digit(s.charAt(i+1), 16));
+       }
+       return data;
+   }
+    
     public static void main(String args[]) { // test     
-   	 String value = "SampleStringToBeEncrypted";
-       String keyval = "thisIsAKey";
+   	 byte[] value = hexStringToByteArray("e04fd020ea3a6910a2d808002b30309d");     
+   	 String keyval = "thisIsAKey";
        
-       int[] encrypted = encrypt(value,keyval);
+       byte[] encrypted = encrypt(value,keyval);
        for(int i = 0; i < encrypted.length; i++)
-           System.out.print((char)encrypted[i]);
+           System.out.print(encrypted[i]);
        System.out.println("");
-       System.out.println(decrypt(encrypted,keyval));        
+       //Turns bytes into Hex
+       StringBuilder sb = new StringBuilder();
+       for (byte b : encrypt(encrypted,keyval)) {
+           sb.append(String.format("%02X ", b));
+       }
+       System.out.println(sb.toString());
+               
    }
 
 
