1726f7209d989bf78daf091b6e5aafeeaee4ad59
Tyle93 <36911253+Tyle93@users.noreply.github.com>
2018-03-05 02:09:28 -0800

    Started adding the confirmation functions

--- receiver.java
+++ receiver.java
@@ -4,53 +4,56 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class receiver extends Thread {
-	
-	private ServerSocket ss;
-	
-	public receiver(int port, boolean asciiArmoring) {
-		try {
-			ss = new ServerSocket(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void run() {
-		while (true) {
-			try {
-				Socket clientSock = ss.accept();
-				saveFile(clientSock);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+public class Receiver extends Thread {
 
-	private void saveFile(Socket clientSock) throws IOException {
-		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
-		FileOutputStream fos = new FileOutputStream("testfile.jpg");
-		byte[] buffer = new byte[4096];
-		
-		int filesize = 15123; // Send file size in separate msg
-		int read = 0;
-		int totalRead = 0;
-		int remaining = filesize;
-		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
-			totalRead += read;
-			remaining -= read;
-			System.out.println("read " + totalRead + " bytes.");
-			fos.write(buffer, 0, read);
-		}
-		
-		fos.close();
-		dis.close();
-	}
+    private ServerSocket ss;
 
-	//this is only to test the receiver class without the driver class
-	public static void main(String[] args) {
-		receiver fs = new receiver(1988, false);
-		fs.start();
-	}
+    public Receiver(int port, boolean asciiArmoring) {
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                Socket clientSock = ss.accept();
+                saveFile(clientSock);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveFile(Socket clientSock) throws IOException {
+        DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+        FileOutputStream fos = new FileOutputStream("testfile.jpg");
+        byte[] buffer = new byte[4096];
+
+        int filesize = 15123; // Send file size in separate msg
+        int read = 0;
+        int totalRead = 0;
+        int remaining = filesize;
+        while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+            totalRead += read;
+            remaining -= read;
+            System.out.println("read " + totalRead + " bytes.");
+            fos.write(buffer, 0, read);
+        }
+
+        fos.close();
+        dis.close();
+    }
+    private void sendConfirmation()
+    {
+
+    }
+    //this is only to test the receiver class without the driver class
+    public static void main(String[] args) {
+        Receiver fs = new Receiver(1988, false);
+        fs.start();
+    }
 
 }
--- sender.java
+++ sender.java
@@ -1,37 +1,89 @@
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class sender {
-	
-	private Socket s;
-	
-	public sender(String host, int port, String file) {
-		try {
-			s = new Socket(host, port);
-			sendFile(file);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-	}
-	
-	public void sendFile(String file) throws IOException {
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-		FileInputStream fis = new FileInputStream(file);
-		byte[] buffer = new byte[4096];
-		
-		while (fis.read(buffer) > 0) {
-			dos.write(buffer);
-		}
-		
-		fis.close();
-		dos.close();	
-	}
-	
-	//this is only to test the sender class without the driver class
-	public static void main(String[] args) {
-		sender fc = new sender("localhost", 1988, "test.txt");
-	}
+public class Sender {
+
+    private Socket s;
+    private ServerSocket inSock;
+    private String host;
+    private int port;
+    private String file;
+    public Sender(String host, int port, String file,int tryCount) {
+        this.host = host;
+        this.port = port;
+        this.file = file;
+        try {
+            s = new Socket(host, port);
+            sendFile(file,tryCount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendFile(String file,int tryCount) throws IOException {
+        int remainingAttempts = tryCount;
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        FileInputStream fis = new FileInputStream(file);
+        byte[] buffer = new byte[4096];
+        File xor = new File("Xor.txt");
+        while (fis.read(buffer) > 0)
+        {
+            byte[] bufferHash = FileCheckSum.hash(buffer);
+            encryptionXor.encrypt(bufferHash,"Key");
+            do{
+                dos.write(buffer);
+                remainingAttempts--;
+                if(remainingAttempts <= 0)
+                {
+                    System.out.println("Exceeded allowed send attempts. Terminating Connection");
+                    fis.close();
+                    dos.close();
+                    s.close();
+                    return;
+                }
+            }while(!waitForResponse());
+
+
+        }
+        fis.close();
+        dos.close();
+    }
+
+    private boolean waitForResponse()
+    {
+        try
+        {
+            ServerSocket servIn = new ServerSocket(port);
+            Socket inSocket = servIn.accept();
+            DataInputStream input = new DataInputStream(inSocket.getInputStream());
+            byte[] in = new byte[256];
+            input.read(in);
+            String s = new String("");
+            for(int i = 0; i < in.length;i++)
+            {
+                s += ((char) in[i]);
+            }
+            if(s.equals("good"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch(IOException ie)
+        {
+            return false;
+        }
+
+    }
+
+    //this is only to test the sender class without the driver class
+    public static void main(String[] args) {
+        Sender fc = new Sender("localhost", 1988, "test.txt",3);
+    }
 
 }
